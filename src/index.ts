aea650cda936bea5185105ccb236eaa884d9ccf7
zafercuz <beltondavid1@gmail.com>
2023-12-14 22:28:45 +0800

    feat: add 3rd new console log for testing

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ interface MyPluginConfig {
 export const myPlugin = definePlugin<MyPluginConfig | void>((config = {}) => {
   console.log('hello from sanity-plugin-sanity-test-publish')
   console.log('2nd test console log')
+  console.log('3rd test console log')
   return {
     name: 'sanity-plugin-sanity-test-publish',
   }
